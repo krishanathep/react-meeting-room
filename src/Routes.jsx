2c66276e7fcdf9c27b3cd2ce32b4daa1afd453c9
Krishanathep <krishanathep@gmail.com>
2023-06-17 11:28:02 +0700

    include routes

---
+++ src/Routes.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import WithNavbar from './layouts/WithNavbar';
+import WithOutnavbar from './layouts/WithOutnavbar';
+import Signin from './pages/auth/Signin'
+import Signup from './pages/auth/Signup'
+import Home from './pages/home'
+import Meeting from './pages/meeting';
+import MeetingVew from './pages/meeting/view'
+import MeetingCreate from './pages/meeting/create'
+import Blogs from './pages/blogs'
+
+const RoutesPage = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route element={<WithOutnavbar/>}>
+          <Route path='/auth/signin' element={<Signin/>}/>
+          <Route path='/auth/signup' element={<Signup/>}/>
+        </Route>
+        <Route element={<WithNavbar/>}>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/meetings/' element={<Meeting/>}/>
+          <Route path='/meetings/view/:id' element={<MeetingVew/>}/>
+          <Route path='/meetings/create' element={<MeetingCreate/>}/>
+
+          <Route path='/blogs' element={<Blogs/>}/>
+        </Route>
+      </Routes>
+    </Router>
+  )
+}
+
+export default RoutesPage
